38453dd30f2819de4697bfe4e4bc16e6aa40a946
Katrina Fuller <trinafuller@att.net>
2019-08-18 14:38:42 -0600

    both range sliders are working!

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-let slider = document.getElementById("tipRange");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value;
+// tip
+let sliderElement = document.querySelector("#tipRange");
+let tipAmountElement = document.querySelector("#tipAmount");
+tipAmountElement.textContent = "Tip % : " + sliderElement.value.toString();
+
 
 //update current slider value each time it is dragged
-slider.oninput = function () {
-  output.innerHTML = this.value;
+function tipPercent() {
+  tipAmountElement.textContent = "Tip % : " + sliderElement.value.toString();
+};
+
+// people
+let peopleElement = document.querySelector("#peopleRange");
+let numberOfPeopleElement = document.querySelector("#peopleAmount");
+numberOfPeopleElement.textContent = "Number of People: " + peopleElement.value.toString();
+
+function peopleRange() {
+  numberOfPeopleElement.textContent = "Number of People: " + peopleElement.value.toString();
 }
